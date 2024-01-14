027cdabc984684b72d4958204f0b9c4f115c9d30
Vankadarisatishkumar <vankadarisatishkumar8074@gmail.com>
2024-01-14 19:30:29 +0530

    running a script for frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -1,16 +1,23 @@
 echo -e "\e[36m installing nginx \e[0m"
 dnf install nginx -y
 
+echo -e "\e[36m copy expense config file \e[0m"
+
 cp expense.conf /etc/nginx/default.d/expense.conf
+echo -e "\e[36m remove old data \e[0m"
 
 rm -rf /usr/share/nginx/html/*
 
+echo -e "\e[36m download frontend application \e[0m"
 
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+
+echo -e "\e[36m extarcting the downloaded application content \e[0m"
 
 
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+echo -e "\e[36m starting nginx service \e[0m"
 
 systemctl enable nginx
 systemctl restart nginx
