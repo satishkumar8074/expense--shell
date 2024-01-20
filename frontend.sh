2e8dc67587bcf16565e055e6e1fd955f0e14ffb6
Vankadarisatishkumar <vankadarisatishkumar8074@gmail.com>
2024-01-20 15:40:12 +0530

    running a script for frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -1,31 +1,28 @@
 log_file=\tmp\expense.logx
 color="\e[36m"
-echo -e ${color} installing nginx \e[0m"
+echo -e "${color} installing nginx \e[0m"
 dnf install nginx -y &>>$log_file
 echo $?
-echo -e ${color} copy expense config file \e[0m"
-
+echo -e "${color} copy expense config file \e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
 echo $?
 
-echo -e ${color} remove old data \e[0m"
+echo -e "${color} remove old data \e[0m"
 
 rm -rf /usr/share/nginx/html/* &>>\tmp\expense.log &>>$log_file
 echo $?
 
 
-echo -e ${color} download frontend application \e[0m"
+echo -e "${color} download frontend application \e[0m"
 
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
 echo $?
 
-echo -e ${color} extarcting the downloaded application content \e[0m"
-
-
+echo -e "${color} changing the user and unzipping the file"
 cd /usr/share/nginx/html &>>$log_file
 unzip /tmp/frontend.zip &>>$log_file $?
 
-echo -e ${color} starting nginx service \e[0m"
+echo -e "${color} starting nginx service \e[0m"
 
 systemctl enable nginx &>>$log_file
 systemctl restart nginx &>>$log_file
