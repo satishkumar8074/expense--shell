3d60957f1ab8ac28e0e541f67c8236c21d93774a
Vankadarisatishkumar <vankadarisatishkumar8074@gmail.com>
2024-01-20 21:52:52 +0530

    running a script for frontend.sh

--- backend.sh
+++ backend.sh
@@ -1,26 +1,42 @@
-dnf module disable nodejs -y
-dnf module enable nodejs:18 -y
-
-dnf install nodejs -y
-
-cp backend.service /etc/systemd/system/backend.service
-
-useradd expense
-
-mkdir /app
-
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
-cd /app
-unzip /tmp/backend.zip
-
-cd /app
-npm install
-
-
-dnf install mysql -y
-
-mysql -h 172.31.36.84 -uroot -pExpenseApp@1 < /app/schema/backend.sql
-
-systemctl daemon-reload
-systemctl enable backend
-systemctl restart backend
+log_file=\tmp\expense.logx
+color="\e[31m"
+echo -e "${color} diabling nodejs \e[0m"
+dnf module disable nodejs -y &>>log_file
+echo $?
+echo -e "${color}  enabling node js \e[0m"
+dnf module enable nodejs:18 -y &>>log_file
+echo $?
+echo -e "${color} installing nodejs  \e[0m"
+dnf install nodejs -y &>>log_file
+echo $?
+echo -e "${color} copying from backend service file  \e[0m"
+cp backend.service /etc/systemd/system/backend.service &>>log_file
+echo $?
+echo -e "${color} adding the user  \e[0m"
+useradd expense &>>log_file
+echo $?
+echo -e "${color} creating the app \e[0m"
+mkdir /app &>>log_file
+echo $?
+echo -e "${color} unzipping the file \e[0m"
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>log_file
+echo $?
+echo -e "${color} changing to the application and unzipping the file  \e[0m"
+cd /app &>>log_file
+unzip /tmp/backend.zip &>>log_file
+echo $?
+echo -e "${color} installing the npm \e[0m"
+cd /app &>>log_file
+npm install &>>log_file
+echo $?
+echo -e "${color}  installing the mysql \e[0m"
+dnf install mysql -y &>>log_file
+echo $?
+echo -e "${color} setting the password to expense app  \e[0m"
+mysql -h 172.31.36.84 -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>log_file
+echo $?
+echo -e "${color} reloading the daemon  \e[0m"
+systemctl daemon-reload &>>log_file
+systemctl enable backend &>>log_file
+systemctl restart backend &>>log_file
+echo $?
--- mysql.sh
+++ mysql.sh
@@ -1,9 +1,19 @@
-dnf module disable mysql -y
+log_file=\tmp\expense.logx
+color="\e[31m"
 
-cp mysql.repo /etc/yum.repos.d/mysql.repo
-
-dnf install mysql-community-server -y
-systemctl enable mysqld
-systemctl restart mysqld
-
-mysql_secure_installation --set-root-pass ExpenseApp@1
+echo -e "${color} disabling default mysql \e[0m"
+dnf module disable mysql -y &>>log_file
+echo $?
+echo -e "${color} copying from the repositories  \e[0m"
+cp mysql.repo /etc/yum.repos.d/mysql.repo &>>log_file
+echo $?
+echo -e "${color} installing the mysql \e[0m"
+dnf install mysql-community-server -y &>>log_file
+echo $?
+systemctl enable mysqld &>>log_file
+echo $?
+systemctl restart mysqld &>>log_file
+echo $?
+echo -e "${color} setting the expense password  \e[0m"
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>log_file
+echo $?
