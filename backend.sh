d0b11809c8bd1ab31c032e0f75ed383cd5610721
Vankadarisatishkumar <vankadarisatishkumar8074@gmail.com>
2024-02-04 00:33:21 +0530

    running a script for frontend.sh

--- backend.sh
+++ backend.sh
@@ -1,44 +1,44 @@
 source common.sh
 
-echo -e "${color} installing nginx \e[0m"
+echo -e "${color} disabling nodejs  \e[0m"
 dnf module disable nodejs -y >>log_file
 
-echo -e "${color} installing nginx \e[0m"
+echo -e "${color} enabling node js \e[0m"
 dnf module enable nodejs:18 -y >>log_file
 if  [ $? -eq 0 ]; then
 echo -e "\e[32m success \e[0m"
 else
 echo -e "\e[33m failure \e[0m"
 fi
-echo -e "${color} installing nginx \e[0m"
+echo -e "${color} installing nodejs \e[0m"
 dnf install nodejs -y >>log_file
 if  [ $? -eq 0 ]; then
 echo -e "\e[32m success \e[0m"
 else
 echo -e "\e[33m failure \e[0m"
 fi
-echo -e "${color} installing nginx \e[0m"
+echo -e "${color}  copying from backend.service \e[0m"
 cp backend.service /etc/systemd/system/backend.service >>log_file
 if  [ $? -eq 0 ]; then
 echo -e "\e[32m success \e[0m"
 else
 echo -e "\e[33m failure \e[0m"
 fi
-echo -e "${color} installing nginx \e[0m"
+echo -e "${color} creating the id \e[0m"
 id expense >>log_file
 if  [ $? -eq 0 ]; then
 echo -e "\e[32m success \e[0m"
 else
 echo -e "\e[33m failure \e[0m"
 fi
-echo -e "${color} installing nginx \e[0m"
+echo -e "${color} adding the expense user \e[0m"
 useradd expense >>log_file
 if  [ $? -eq 0 ]; then
 echo -e "\e[32m success \e[0m"
 else
 echo -e "\e[33m failure \e[0m"
 fi
-echo -e "${color} installing nginx \e[0m"
+echo -e "${color} changing the directory \e[0m"
 mkdir /app >>log_file
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip >>log_file
 if  [ $? -eq 0 ]; then
@@ -46,7 +46,7 @@ echo -e "\e[32m success \e[0m"
 else
 echo -e "\e[33m failure \e[0m"
 fi
-echo -e "${color} installing nginx \e[0m"
+echo -e "${color} changing the directory and unzipping the file \e[0m"
 cd /app >>log_file
 unzip /tmp/backend.zip >>log_file
 if  [ $? -eq 0 ]; then
